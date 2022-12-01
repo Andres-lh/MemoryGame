0930837a76aee34d92f51ffe20cac9cea533b162
rafepoor24 <105663364+rafepoor24@users.noreply.github.com>
2022-12-01 17:30:25 -0500

    Table

---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField] private int m_SizeTableX = 4;
+    [SerializeField] private int m_SizeTableY = 4;
+    [SerializeField] Vector2 m_SplitTokens = Vector2.zero;
+
+
+    [Header("References")]
+    [SerializeField] private GameObject m_Token;
+    [SerializeField] private Transform m_AreaOfGame; // defined area of game in order to instance tokens and adjust it.
+
+    void Start()
+    {
+        for (int x = 0; x < m_SizeTableX; x++)
+        {
+            for (int y = 0; y < m_SizeTableY; y++)
+            {
+                GameObject tokenGo = Instantiate(m_Token);
+                tokenGo.transform.SetParent(m_AreaOfGame);
+                tokenGo.transform.localPosition = new Vector3(x * m_SplitTokens.x, 0, y * m_SplitTokens.y);
+                Debug.Log(x);
+            }
+
+        }
+
+    }
+}
+
+
+
